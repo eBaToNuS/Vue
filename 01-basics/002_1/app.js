d7b21272fdabf2ce758598ae604c8efb89afcb91
eBaToNuS <Sulzhenko_10012004@mail.ru>
2025-03-11 17:20:26 +0300

    Complete first module

--- 01-basics/002_1/app.js
+++ 01-basics/002_1/app.js
@@ -1,9 +1,36 @@
 const App = {
   data() {
     return {
-      counter: 3,
-      appTitle: "Счетчик",
+      appTitle: "Заметки",
+      myPlaceholder: "Введите текст",
+      inputValue: "",
+      notes: [],
     };
+  },
+  methods: {
+    addNewNote() {
+      if (this.inputValue !== "") {
+        this.notes.push(this.inputValue);
+        this.inputValue = "";
+      }
+    },
+    deleteButton(index) {
+      this.notes.splice(index, 1);
+      console.log(index);
+    },
+    toUpperCase(item) {
+      return item.toUpperCase();
+    },
+  },
+  computed: {
+    doubleCount() {
+      return this.notes.length * 2;
+    },
+  },
+  watch: {
+    inputValue(value) {
+      if (this.inputValue.length > 10) this.inputValue = "";
+    },
   },
 };
 
