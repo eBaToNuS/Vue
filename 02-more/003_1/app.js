de4925031dad6993bd4f6a2a89ce0bcd2f1da513
eBaToNuS <Sulzhenko_10012004@mail.ru>
2025-03-13 14:04:36 +0300

    13.03

--- 02-more/003_1/app.js
+++ 02-more/003_1/app.js
@@ -7,6 +7,17 @@ Vue.createApp({
       lastName: "Sulzhenko",
       age: 21,
     },
-    arr: [1, 2, 3, 4, 5, 6],
+    arr: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
   }),
+  methods: {
+    addItems(event) {
+      this.addItems.unshift(this.$refs.myInput.value);
+      this.$refs.myInput.value = "";
+    },
+  },
+  computed: {
+    evenItems() {
+      return this.arr.filter((item) => item % 2 === 0);
+    },
+  },
 }).mount("#app");
