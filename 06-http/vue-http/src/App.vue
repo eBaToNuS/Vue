f3189c2e6cc3d61807929114bf0350849449fd78
eBaToNuS <Sulzhenko_10012004@mail.ru>
2025-03-31 17:00:21 +0300

    31.03

--- 06-http/vue-http/src/App.vue
+++ 06-http/vue-http/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <app-alert :alert="alert" @close="alert = null"></app-alert>
     <form class="card" @submit.prevent="createPerson">
       <h2>Работа с базой данных</h2>
 
@@ -17,17 +18,40 @@
         Создать человека
       </button>
     </form>
+    <app-loader v-if="loading"></app-loader>
+    <app-people-list
+      v-else
+      @remove="removePerson"
+      :people="people"
+      @load="loadPeople"
+    ></app-people-list>
   </div>
 </template>
 
 <script>
+import AppPeopleList from "./AppPeopleList.vue";
+import axios from "axios";
+import AppAlert from "./AppAlert.vue";
+import AppLoader from "./AppLoader.vue";
 export default {
   data() {
     return {
+      people: [],
       name: "",
+      alert: null,
+      loading: false,
     };
   },
+  mounted() {
+    this.loadPeople();
+  },
   methods: {
+    async removePerson(id) {
+      await axios.delete(
+        `https://vue-stage-http-default-rtdb.asia-southeast1.firebasedatabase.app/people/${id}.json`
+      );
+      this.people.splice(this.people.indexOf(id), 1);
+    },
     async createPerson() {
       console.log(this.name);
       // https://vue-stage-http-default-rtdb.asia-southeast1.firebasedatabase.app/people.json
@@ -42,8 +66,41 @@ export default {
         }
       );
       const firebaseData = await response.json();
+      this.people.push({
+        firstName: this.name,
+        id: firebaseData.name,
+      });
+      this.loadPeople();
       console.log(firebaseData);
+      this.name = "";
     },
+    async loadPeople() {
+      try {
+        this.loading = true;
+
+        const { data } = await axios.get(
+          "https://vue-stage-http-default-rtdb.asia-southeast1.firebasedatabase.app/people.json"
+        );
+        this.people = Object.keys(data).map((key) => {
+          return { id: key, ...data[key] };
+        });
+        this.loading = false;
+      } catch (e) {
+        this.loading = false;
+        this.alert = {
+          type: "danger",
+          title: "Пользователей нет",
+          text: "Введите первого пользователя",
+        };
+
+        console.log(e.message);
+      }
+    },
+  },
+  components: {
+    AppPeopleList,
+    AppAlert,
+    AppLoader,
   },
 };
 </script>
---
+++ 06-http/vue-http/src/AppPeopleList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-if="people.length !== 0">
+    <div class="card inline" :key="person.id" v-for="person in people">
+      <h3>{{ person.firstName }}</h3>
+      <button class="btn danger" @click="$emit('remove', person.id)">
+        Удалить
+      </button>
+    </div>
+  </div>
+  <div class="card center" v-else>
+    <h4>Людей пока нет.</h4>
+    <button class="btn primary" @click="$emit('load')">Загрузить список</button>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ["load", "remove"],
+  props: ["people"],
+};
+</script>
+
+<style lang="css" scoped>
+.inline {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
